4509034d4fffb09c05c6b8cc51dc007df18e1df5
Jeron Chen <jeronchen@gmail.com>
2023-08-09 16:43:58 -0400

    Update installing_ef2.sh

--- installing_ef2.sh
+++ installing_ef2.sh
@@ -13,9 +13,17 @@ rm Miniconda3-latest-Linux-x86_64.sh
 
 export PATH="${CONDAFOLDDIR}/conda/condabin:${PATH}"
 eval "$(conda shell.bash hook)"
-conda create -p $CONDAFOLDDIR/esmfold-conda python=3.10 -y
+conda create -f environmental.yml -y
 conda activate $CONDAFOLDDIR/esmfold-conda
 conda update -n base conda -y
+
+
+pip install torch torchvision torchaudio
+
+pip install "fair-esm[esmfold]"
+# OpenFold and its remaining dependency
+pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
+pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059694619831a7db195b7e0988fc4ff3a307'
 
 sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
